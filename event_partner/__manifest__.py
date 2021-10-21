9030cae61fced73d40f14cefe9d0a1a6d06e61a1
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-21 13:57:50 +0200

    Update __manifest__.py

--- event_partner/__manifest__.py
+++ event_partner/__manifest__.py
@@ -29,6 +29,7 @@
     'category': 'Employee',
     'version': '14.0.0.2',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'license': 'AGPL-3',
     'website': 'https://vertel.se',
     'description': """
         To be able to have multiple partners on event. \n
