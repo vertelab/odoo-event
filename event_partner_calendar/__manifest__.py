30fc101e0660d6d53bc2b5b2c626a061f0a20ced
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2021-10-22 08:49:32 +0200

    Update __manifest__.py

--- event_partner_calendar/__manifest__.py
+++ event_partner_calendar/__manifest__.py
@@ -1,7 +1,7 @@
 ##############################################################################
 #
-#    OpenERP, Open Source Management Solution, third party addon
-#    Copyright (C) 2004-2021 Vertel AB (<https://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -14,7 +14,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
