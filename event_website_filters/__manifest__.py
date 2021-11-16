6d8db7262809792303ba05aa67f0a2cf234623f0
iammiracle <miracleaayodele@gmail.com>
2021-11-16 17:15:44 +0100

    added more filters to event website

---
+++ event_website_filters/__manifest__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Event Website Filters',
+    'summary': 'Event Website Filters',
+    'author': 'Vertel AB',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'category': 'Employee',
+    'version': '14.0.0.0',
+    'license': 'AGPL-3',
+
+    'description': """
+        14.0.0.0
+           -added filters to Website events.   
+    """,
+    'depends': ['website_event', 'event'],
+    'data': [
+        'views/event_template.xml',
+    ],
+    'application': False,
+    'installable': True,
+}
+# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
