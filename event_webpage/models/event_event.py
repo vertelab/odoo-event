ec7b94e4fcddcf74ae2af5b934d6540a90a53f67
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2021-11-24 13:56:46 +0000

    event_webpage: Moved description from registration page to introduction page,this was harder then it should be.

--- event_webpage/models/event_event.py
+++ event_webpage/models/event_event.py
@@ -2,6 +2,7 @@ from odoo import models, fields, api, _
 from odoo.tools.translate import html_translate
 import logging
 _logger = logging.getLogger(__name__)
+from odoo.addons.http_routing.models.ir_http import slug
 
 class EventEventType(models.Model):
         _inherit = 'event.type'
@@ -14,11 +15,11 @@ class EventEventType(models.Model):
         
         @api.onchange("event_type_ticket_ids")
         def _set_default_ticket_description(self):
+            self.ticket_description = ""
             _logger.warning(f"{self.description=}")
-            if self.event_type_ticket_ids:
-                self.ticket_description = self.event_type_ticket_ids[0].product_id.description_sale
-            else:
-                self.ticket_description = ""
+            if 'product_id' in self.env['event.event.ticket']._fields:
+                if self.event_type_ticket_ids and self.event_ticket_ids[0].product_id:
+                    self.ticket_description = self.event_type_ticket_ids[0].product_id.description_sale
 
 
 class EventEvent(models.Model):
@@ -26,14 +27,23 @@ class EventEvent(models.Model):
         
         ticket_description= fields.Text(default="There is no description", compute='_set_default_ticket_description')
         
+        empty_description= fields.Text(default=" ", string="Is used for the description view since we want it to be empty, and if I remove it the formating breaks,so we have this useless empty string, go team!")
         @api.onchange("event_type_id")
         def _set_decription_to_event_type_description(self):
             self.description = self.event_type_id.description
+        
+        # ~ @api.depends("event_type_id")
+            # ~ def _set_decription_to_event_type_description(self):
+                # ~ self.description = self.event_type_id.description
 
 
         @api.onchange("event_ticket_ids")
         def _set_default_ticket_description(self):
-            if self.event_ticket_ids:
-                self.ticket_description = self.event_ticket_ids[0].product_id.description_sale
-            else:
-                self.ticket_description = ""
+            self.ticket_description = ""
+            if 'product_id' in self.env['event.event.ticket']._fields:
+                if self.event_ticket_ids and self.event_ticket_ids[0].product_id:
+                    self.ticket_description = self.event_ticket_ids[0].product_id.description_sale
+        
+        
+
+
