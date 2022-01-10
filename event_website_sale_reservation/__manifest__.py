a7f1ba561fd881842b4149a7c8306966aa2b42be
Emanuel Bergsten <emanuel.bergsten@vertel.se>
2022-01-10 10:37:09 +0000

    event_website_sale_reservation: Created
    * Added 'incl/excl VAT' for event sales on website
    * Added translation to Swedish

---
+++ event_website_sale_reservation/__manifest__.py
@@ -0,0 +1,21 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Website Event Sale Reservations",
+    "summary": "Add VAT information to event sale page",
+    "version": "14.0.1.0.0",
+    "development_status": "Beta",
+    "category": "Marketing",
+    "website": "https://github.com/vertelab/odoo-event",
+    "author": "Vertelab",
+    "maintainers": ["Vertelab"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "website_event_sale",
+    ],
+    "data": [
+        "views/event_website_sale_reservation.xml",
+    ],
+}
