a4ea3148acc3cef79c788853671eddf20802c71b
Daniel Löfgren <dan.lofgrens@gmail.com>
2022-02-08 08:50:17 +0000

    Added chatter to events.

---
+++ event_chatter/models/event_event.py
@@ -0,0 +1,11 @@
+# Copyright 2021 Tecnativa - Jairo Llopis
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from email.policy import default
+from odoo import fields, models, _
+
+
+class EventEvent(models.Model):
+    _name = "event.event"
+    _inherit = ["event.event", 'mail.activity.mixin', 'mail.thread']
+    
