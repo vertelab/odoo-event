f4777c0d03ed391d3c073a7aa7823504195c7ac1
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-30 11:43:44 +0100

    Update __manifest__.py

--- event_partner_user_info/__manifest__.py
+++ event_partner_user_info/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -14,28 +14,39 @@
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Affero General Public License for more details.
 #
-
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    'name': 'Event Partner Max Users',
+    'name': 'Event: Partner User Info',
     'version': '14.0.0.1',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Extends the event registration form view with visible information about the size of the event.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
     'category': 'Event',
-    'summary': 'Extends the event registration form view with visible information about the size of the event',
     'description': """
         Extends the event registration form view with visible information about the size of the event\n\n
         Features:\n
             *   Adds visible information about the size of the event.\n
         This module is maintained from: https://github.com/vertelab/odoo-event/tree/14.0/event_partner_max_users/\n
     """,
+    #'sequence': '1',
     'author': 'Vertel AB',
-    'website': 'https://www.vertel.se',
+    'website': 'https://vertel.se/apps/odoo-event/event_partner_user_info',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-event',
+    # Any module necessary for this one to work correctly
     'depends': ['event','website_event'],
     'data': [
         'views/templates.xml',
     ],
     'installable': True,
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
