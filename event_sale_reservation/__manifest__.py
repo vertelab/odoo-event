451ddca988727552a463c0cb3036367a27f5be52
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-30 14:23:45 +0100

    Update __manifest__.py

--- event_sale_reservation/__manifest__.py
+++ event_sale_reservation/__manifest__.py
@@ -1,18 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 # Copyright 2021 Tecnativa - Jairo Llopis
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Sell event reservations",
-    "summary": "Allow selling event registrations before the event exists",
-    "version": "14.0.1.1.0",
-    "development_status": "Beta",
-    "category": "Marketing",
-    "website": "https://github.com/OCA/event",
-    "author": "Tecnativa, Odoo Community Association (OCA)",
-    "maintainers": ["Yajo", "Vertelab"],
-    "license": "AGPL-3",
-    "application": False,
-    "installable": True,
+    'name': 'Event: Sale Reservation',
+    'version': '14.0.1.1.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Allow selling event registrations before the event exists.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Event',
+    'description': """
+    To sell event reservations.
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-event/event_sale_reservation',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer':  ['Yajo', 'Vertel AB'],
+    'repository': 'https://github.com/vertelab/odoo-event',
+    # Any module necessary for this one to work correctly
     "depends": [
         "event_sale",
         "website_event",
@@ -34,3 +63,4 @@
     # and not sum of qtys; integrating with them would require a glue module
     "excludes": ["event_registration_multi_qty", "event_sale_registration_multi_qty"],
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
