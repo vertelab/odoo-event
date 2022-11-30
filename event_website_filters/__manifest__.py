e5a7af95be36a2d8168d04f40213cb97fe1a1931
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-30 16:32:46 +0100

    Update __manifest__.py

--- event_website_filters/__manifest__.py
+++ event_website_filters/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,16 +20,27 @@
 ##############################################################################
 
 {
-    'name': 'Event Website Filters',
-    'summary': 'Event Website Filters',
-    'author': 'Vertel AB',
-    'category': 'Employee',
-	"contributor": "Miracle Ayodele,Fredrik Arvas",
+    'name': 'Event: Website Filters',
     'version': '14.0.1.0',
-    'license': 'AGPL-3',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': '',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Event',
     'description': """
-        14.0.1.0 Added translation to swedish\n 
+        14.0.1.0 -- Added translation to swedish. \n 
     """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-event/event_website_filters',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': 'Miracle Ayodele, Fredrik Arvas',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-event',
+    # Any module necessary for this one to work correctly
+
     'depends': ['website_event', 'event'],
     'data': [
         'views/event_template.xml',
@@ -37,4 +48,4 @@
     'application': False,
     'installable': True,
 }
-# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
