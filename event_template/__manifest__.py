635ec4f8565b983084d106ee1ead4b716a7a38f3
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-30 14:29:15 +0100

    Update __manifest__.py

--- event_template/__manifest__.py
+++ event_template/__manifest__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Odoo, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<http://vertel.se>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -15,26 +15,33 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 
 {
-    'name': 'Event Template',
-    'summary': 'To be able to create event from another event.',
-    'author': 'Vertel AB',
-    'contributor': 'Mitchell Admin',
-    'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-event.git',
-    'category': 'Employee',
+    'name': 'Event: Event Template',
     'version': '14.0.1.3.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se/',
+    'summary': 'To be able to create event from another event.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Event',
     'description': """
         To be able to create event from another event. \n
         Depreciated since there is a template-function in the core-module.\n
     """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-event/event_template',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-event',
+    # Any module necessary for this one to work correctly
+
     'depends': ['website_event', 'event', 'base_tier_validation', 'event_partner'],
     'data': [
         'views/event_event_view.xml'
