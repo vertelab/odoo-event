6eb85d0dfc48554a33e6b2dd0d86207869969c92
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-11-30 13:39:24 +0100

    Update __manifest__.py

--- event_reservation_gender/__manifest__.py
+++ event_reservation_gender/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,17 +20,28 @@
 ##############################################################################
 
 {
-    'name': 'Event Reservation Gender',
-    'summary': 'Adds Gender to Event Reservation',
-    'author': 'Vertel AB',
-    'category': 'Event',
+    'name': 'Event: Reservation Gender',
     'version': '14.0.0.1',
-    'license': 'AGPL-3',
-    'website': 'https://vertel.se',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Adds Gender to Event Reservation.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Event',
     'description': """
-        14.0.0.1
-            - Added gender to registration pivot view and registration form view
+    14.0.0.1
+        - Added gender to registration pivot view and registration form view
     """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-event/event_reservation_gender',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-event',
+    # Any module necessary for this one to work correctly
+
     'depends': ['website_event', 'event', 'base'],
     'data': [
         'views/event_registration_views.xml',
@@ -38,4 +49,4 @@
     'application': False,
     'installable': True,
 }
-# vim:expandtab:smartindent:tabstop=4s:softtabstop=4:shiftwidth=4:
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
