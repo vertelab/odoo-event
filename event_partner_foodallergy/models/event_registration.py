b4b1ea90cce6fcb946238db1b112a11c6d7cd68a
olyrhc <robin.calvin@vertel.se>
2023-06-19 12:58:36 +0000

    event_partner_foodallergy: Added a 'special_food' compute field to the event registration tree view

--- event_partner_foodallergy/models/event_registration.py
+++ event_partner_foodallergy/models/event_registration.py
@@ -7,9 +7,19 @@ class EventEvent(models.Model):
 class EventRegistration(models.Model):
     _inherit = "event.registration"
     food_allergy = fields.Char(string=_("Food allergy"))
+    special_food = fields.Boolean(string=_('Special food'), compute="_has_food_allergy", readonly=True)
     food_is_served = fields.Boolean(related = "event_id.food_is_served")
 
     def _get_website_registration_allowed_fields(self):
         res = super(EventRegistration,self)._get_website_registration_allowed_fields()
         res.add('food_allergy')
         return res
+
+    def _has_food_allergy(self):
+        if self.food_allergy:
+            if len(self.food_allergy) > 0:
+                self.special_food = True
+            else:
+                self.special_food = False
+        else:
+            self.special_food = False
