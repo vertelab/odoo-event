c787eb168e0e317fe628a73fac16def0419c5a2a
Björn Hayer <bjorn.hayer@vertel.se>
2023-08-25 09:48:49 +0200

    Webside event sale status: Claned up code and added more friendly warnings.

--- website_event_sale_status/models/event_registration.py
+++ website_event_sale_status/models/event_registration.py
@@ -10,11 +10,12 @@ class EventRegistrationCron(models.Model):
     def cron_remove_tentative_registrations(self):
         registration_search = self.env['event.registration'].search([('visitor_id', '!=', False), ('state', '=', 'draft')])
         registration_search_filtered = registration_search.filtered(lambda x: x.event_id.stage_id.name in ('Ny', 'Publicerad', 'Inbjudan skickad') and x.event_id.active == True)
-        _logger.warning(f"{registration_search_filtered=}")
+        _logger.warning(f"The following event.registration are deemed 'incomplete' ones, and will be removed if they are too old: {registration_search_filtered}")
         for registration in registration_search_filtered:
             if registration.date_open <= datetime.datetime.utcnow() - datetime.timedelta(hours=1):
-                registration.action_cancel()
-                registration.sale_order_id.state = "cancel"
-                #registration.active = False
-                #registration.sale_order_line_id.unlink()
-                #registration.sale_order_id.active = False
+                if registration.sale_order_id.invoice_ids:
+                    _logger.error(f"This event.registration was deemed 'incomplete' and too old, but had a invoice, and could not be removed: {registration}")
+                else:
+                        _logger.warning(f"This event.registration was deemed 'incomplete' and too old, and canceled: {registration}")
+                        registration.action_cancel()
+                        registration.sale_order_id.state = "cancel"
