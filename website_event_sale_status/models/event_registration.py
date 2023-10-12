d9cff8d01958670528c6837069193a40ab2abd82
Björn Hayer <bjorn.hayer@vertel.se>
2023-10-12 13:28:10 +0200

    website_event_sale_satus: Temporary testing change

--- website_event_sale_status/models/event_registration.py
+++ website_event_sale_status/models/event_registration.py
@@ -12,7 +12,8 @@ class EventRegistrationCron(models.Model):
         registration_search_filtered = registration_search.filtered(lambda x: x.event_id.stage_id.name in ('Ny', 'Publicerad', 'Inbjudan skickad') and x.event_id.active == True)
         _logger.warning(f"The following event.registration are deemed 'incomplete' ones, and will be removed if they are too old: {registration_search_filtered}")
         for registration in registration_search_filtered:
-            if registration.date_open <= datetime.datetime.utcnow() - datetime.timedelta(hours=1):
+            #if registration.date_open <= datetime.datetime.utcnow() - datetime.timedelta(hours=1):
+            if registration.date_open <= datetime.datetime.utcnow() - datetime.timedelta(minutes=5):
                 if registration.sale_order_id.invoice_ids:
                     _logger.error(f"This event.registration was deemed 'incomplete' and too old, but had a invoice, and could not be removed: {registration}")
                 else:
