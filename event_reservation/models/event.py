7c25de0dfd52e0ee5cb84f2b684e3168b7542c87
iammiracle <miracleaayodele@gmail.com>
2023-10-23 16:00:47 +0200

    dont send reminder to event reservations

--- event_reservation/models/event.py
+++ event_reservation/models/event.py
@@ -47,3 +47,12 @@ class EventReservationTicket(models.Model):
                 ticket.seats_available_event_limit = ticket.event_id.seats_available
             else:
                 ticket.seats_available_event_limit = 0
+
+
+class EventEvent(models.Model):
+    _inherit = 'event.event'
+
+    def mail_attendees(self, template_id, force_send=False, filter_func=lambda self: self.state not in ['cancel', 'reservation']):
+        for event in self:
+            for attendee in event.registration_ids.filtered(filter_func):
+                self.env['mail.template'].browse(template_id).send_mail(attendee.id, force_send=force_send)
