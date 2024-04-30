b53c836ac88e1a213123e05dd6233d95d8afee3d
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-04-30 08:55:20 +0200

    Update __manifest__.py

--- event_so_trigger/__manifest__.py
+++ event_so_trigger/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Event: Trigger Email Based on Sale Order',
-    'version': '14.0.1.3.0',
+    'version': '17.0.1.3.0',
     'summary': 'Trigger Email Based on Sale Order.',
     'category': 'Event',
     'description': """
