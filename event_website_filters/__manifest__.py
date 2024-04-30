874dc8e59556af4ac3718212b55462eb023bd61e
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-04-30 08:57:07 +0200

    Update __manifest__.py

--- event_website_filters/__manifest__.py
+++ event_website_filters/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Event: Website Filters',
-    'version': '14.0.1.0',
+    'version': '17.0.1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': '',
     # Categories can be used to filter modules in modules listing
