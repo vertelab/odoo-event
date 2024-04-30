f25289bdfd91d68c0b3ec0c40848788c0e151031
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-04-30 08:57:57 +0200

    Update __manifest__.py

--- website_event_user_account/__manifest__.py
+++ website_event_user_account/__manifest__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Event: Website Event User Account',
     'version': '14.0.0.1.0',
-    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    # Version ledger: 17.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Create portal account for participants of an event.',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
