e9e365f6afa35ce37e7162d996e5820bf991ee00
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-04-30 08:47:54 +0200

    Update __manifest__.py

--- event_attendee_badges/__manifest__.py
+++ event_attendee_badges/__manifest__.py
@@ -20,8 +20,8 @@
 ##############################################################################
 
 {
-    'name': 'Event Attendee Namebadges',
-    'version': '14.0.1.0',
+    'name': 'Event: Attendee Namebadges',
+    'version': '17.0.1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': '',
     'category': 'Event',
