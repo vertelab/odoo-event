c2015dbb9463067232d76598d531b9a5096d2abc
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-04-30 08:56:33 +0200

    Update __manifest__.py

--- event_type_webpage/__manifest__.py
+++ event_type_webpage/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Event: Event Type Webpage',
-    'version': '14.0.0.1.0',
+    'version': '17.0.0.1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Allows for more control of the appearance of an using event templates.',
     # Categories can be used to filter modules in modules listing
