7e5d3bd3e541b5c55bb4f4296726d072c2af2637
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2024-05-02 11:14:35 +0000

    event_elearning: module

--- event_elearning/__manifest__.py
+++ event_elearning/__manifest__.py
@@ -2,7 +2,7 @@
 ##############################################################################
 #
 #    Odoo SA, Open Source Management Solution, third party addon
-#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
+#    Copyright (C) 2021- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -20,32 +20,32 @@
 ##############################################################################
 
 {
-    'name': 'Event: HR',
-    'version': '17.0.0.1',
+    'name': 'Account: Period',
+    'version': '17.0.0.0.2',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'summary': 'Adds some HR functions to event .',
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Event',
-    'description': """
-    - Added Mandatory and other courses to a job position
-    """,
-    #'sequence': '1',
+    'summary': 'Divide the year in 12 months or 4 quarters.',
+    'category': 'Accounting',
+    #'sequence': '1'
     'author': 'Vertel AB',
-    'website': 'https://vertel.se/apps/odoo-event/event_hr',
-    'images': ['static/description/banner.png'], # 560x280 px.
+    'website': 'https://vertel.se/apps/odoo-account/account_period',
+    'images': ['/static/description/banner.png'], # 560x280 px.
     'license': 'AGPL-3',
     'contributor': '',
     'maintainer': 'Vertel AB',
-    'repository': 'https://github.com/vertelab/odoo-event',
-    # Any module necessary for this one to work correctly
-    'depends': ['hr', 'event', ],
+    'repository': 'https://github.com/vertelab/odoo-account',
+    'description': """
+Account Period
+==============
+Added period for accounting. Either 12 months or 4 quarters.
+    """,
+    'depends': ['event','website_slides'],
     'data': [
-#	'security/ir.model.access.csv',
-        'views/hr_job_views.xml',
+        'views/event.xml',
     ],
-    'application': False,
+    'demo': [],
+    'qweb': [],
     'installable': True,
+    'application': False,
+    'auto_install': False,
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
