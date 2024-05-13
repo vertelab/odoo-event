19cd7aa4a15ee2db82927c89a82bde8af88331f3
Anders Wallenquist <anders.wallenquist@vertel.se>
2024-05-13 16:27:33 +0000

    event_waitinglist: new triggers

--- event_waitlist_website/models/event_waiting_list.py
+++ event_waitlist_website/models/event_waiting_list.py
@@ -1,5 +1,8 @@
 from odoo import models, fields, _, api
 
+class EventMail(models.Model):
+    _inherit = "event.mail"
+    interval_type = fields.Selection(selection_add=[("waiting_list", "Skicka till Väntelistan"),("unreserve_mandatory","Skicka mail om avregistrering")], ondelete={"waiting_list": "set default","unreserve_mandatory": "set default"})
 
 class EventWaitingList(models.Model):
     _name = 'event.waiting.list'
