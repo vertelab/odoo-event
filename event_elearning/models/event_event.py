978ce9a88fb2181e91ea6aa0f86bd91838056f50
marcuszxc <marcuszxc10@gmail.com>
2024-05-15 12:01:22 +0000

    Moved everything to do with waitlist to the event_waitlist_website module

--- event_elearning/models/event_event.py
+++ event_elearning/models/event_event.py
@@ -1,30 +1,10 @@
-import logging
-from odoo import models, fields, api, _, SUPERUSER_ID
-
-_logger = logging.getLogger(__name__)
+from odoo import models, fields, api, _
 
 class EventEvent(models.Model):
     _inherit = 'event.event'
 
     slide_channel_id = fields.Many2one(
         string="Slide Channel", comodel_name="slide.channel", compute='_compute_slide_channel', precompute=True, readonly=False, store=True)
-    
-    full_website_url = fields.Char(compute="_compute_full_website_url_field", string="The actuall full url for the website")
-
-    @api.depends("full_website_url")
-    def _compute_full_website_url_field(self):
-
-        base_url = self.env['ir.config_parameter'].get_param('web.base.url')
-
-        for event in self:
-
-            event.full_website_url = False
-
-            event_url = event.website_url
-
-            if base_url and event_url:
-
-                event.full_website_url = f"{base_url}{event_url}"
 
     @api.depends('event_type_id')
     def _compute_slide_channel(self):
@@ -32,24 +12,4 @@ class EventEvent(models.Model):
             if event.event_type_id.slide_channel_id:
                 event.slide_channel_id = event.event_type_id.slide_channel_id
 
-    @api.model_create_multi
-    def create(self, vals_list):
-        
-        _logger.warning("event create"*100)
-        events = super(EventEvent, self).create(vals_list)
-
-        for event in events:
-            
-            waiting_list_ids = self.env['event.waiting.list'].search([('event_type_id', '=', event.event_type_id.id)])
-            _logger.warning(f"waiting_list_ids {waiting_list_ids=}")
-            
-            if not waiting_list_ids:
-                return
-            
-            _logger.warning(f"event {event=}")
-            onsubscribe_schedulers = event.event_mail_ids.filtered(lambda event_mail: event_mail.interval_type == 'open_event_slot')
-            
-            _logger.warning(f"onsubscribe_schedulers {onsubscribe_schedulers=}")
-            onsubscribe_schedulers.with_user(SUPERUSER_ID).execute()
-
-        return events
+    
