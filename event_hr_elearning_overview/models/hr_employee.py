6413aa2bb78fa376bc8f637b284db80cae698cd2
iammiracle <miracleaayodele@gmail.com>
2024-06-27 14:43:26 +0200

    hide course overview when there is no course

--- event_hr_elearning_overview/models/hr_employee.py
+++ event_hr_elearning_overview/models/hr_employee.py
@@ -2,32 +2,36 @@
 
 from odoo import models, fields, api
 import logging
+
 _logger = logging.getLogger(__name__)
+
+
 class HrEmployee(models.Model):
-     _inherit="hr.employee"
-     
-    #  subordinate_course_participations_ids = fields.One2many('slide.channel.partner', string='Subordinate course participations',compute="")
-    #  subordinate_course_participations_amount = fields.Integer(string="Number of subordinate course participations")
-     
-    #  def compute_subordinate_course_participations(self):
-    #      subordinate_subscribed_courses = self.child_ids.subscribed_courses
-    #      subordinate_partner_ids = self.child_ids.user_partner_id
-    #      #subordinate_course_participations = self.env['slide.channel.partner'].search([('partner_id.id','in',[subordinate_partner_ids.ids])]) 
-    #      subordinate_course_participations = self.env['slide.channel.partner'].search([('partner_id.id','in',subordinate_partner_ids.ids)]).ids
+    _inherit = "hr.employee"
 
-     def manager_course_overview(self):
-         subordinate_partner_ids = self.child_ids.user_partner_id
-         #subordinate_course_participations = self.env['slide.channel.partner'].search([('partner_id.id','in',[subordinate_partner_ids.ids])]) 
-         subordinate_course_participations = self.env['slide.channel.partner'].search([('partner_id.id','in',subordinate_partner_ids.ids)]).ids
-         
+    @api.depends('subordinate_course_participants_ids', 'name')
+    def compute_subordinate_course_participants(self):
+        for rec in self:
+            subordinate_partner_ids = self.child_ids.user_partner_id
+            subordinate_course_participants = self.env['slide.channel.partner'].search(
+                [('partner_id.id', 'in', subordinate_partner_ids.ids)]).ids
+            rec.subordinate_course_participants_ids = subordinate_course_participants
+            rec.subordinate_course_participants_count = len(rec.subordinate_course_participants_ids)
 
-         
-         return {
+    subordinate_course_participants_ids = fields.One2many('slide.channel.partner', 'partner_id',
+                                                          string='Subordinate Course Participants',
+                                                          compute='compute_subordinate_course_participants')
+
+    subordinate_course_participants_count = fields.Integer(string="Number of subordinate course Participants",
+                                                           compute='compute_subordinate_course_participants')
+
+    def manager_course_overview(self):
+        return {
             'type': 'ir.actions.act_window',
-            'name': 'Subordinate Course Participations',
+            'name': 'Subordinate Course Participation',
             'view_mode': 'kanban,form',
             'res_model': 'slide.channel.partner',
-            'domain': [('id', 'in', subordinate_course_participations)],
+            'domain': [('id', 'in', self.subordinate_course_participants_ids.ids)],
             'context': {'group_by': 'partner_id'},
             'target': 'current',
         }
