432afa74063395729e07a0ddc4ce5ef9516924ec
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-20 08:38:21 +0200

    Update __manifest__.py

--- event_hr_elearning_overview/__manifest__.py
+++ event_hr_elearning_overview/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) {year} {company} (<{mail}>)
-#    All Rights Reserved
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2024- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,15 +15,15 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
 #
 # https://www.odoo.com/documentation/14.0/reference/module.html
 #
 {
-    'name': 'HR ELearning Overview',
-    'version': '17.0.0.0.0',
+    'name': 'Event: HR ELearning Overview',
+    'version': '0.1',
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.odoo.com""",
