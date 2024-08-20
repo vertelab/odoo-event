0e54190b8f37fb351e28cbbf2d160ea9cc6c7d06
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-20 08:54:35 +0200

    Update __manifest__.py

--- event_hr_elearning_overview/__manifest__.py
+++ event_hr_elearning_overview/__manifest__.py
@@ -24,12 +24,10 @@
 {
     'name': 'Event: HR ELearning Overview',
     'version': '0.1',
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.odoo.com""",
+    'summary': '',
     'category': 'Event', # Technical Settings|Localization|Payroll Localization|Account Charts|User types|Invoicing|Sales|Human Resources|Operations|Marketing|Manufacturing|Website|Theme|Administration|Appraisals|Sign|Helpdesk|Administration|Extra Rights|Other Extra Rights|
     'description': """
-        Long description of module's purpose
+    
     """,
     #'sequence': 1,
     'author': 'Vertel AB',
