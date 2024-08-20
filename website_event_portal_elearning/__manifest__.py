1695c239c587fea28aaa0aa46bb1650caeba87d2
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-20 08:52:50 +0200

    Update __manifest__.py

--- website_event_portal_elearning/__manifest__.py
+++ website_event_portal_elearning/__manifest__.py
@@ -27,9 +27,9 @@
     'summary': """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.odoo.com""",
-    'category': '', # Technical Settings|Localization|Payroll Localization|Account Charts|User types|Invoicing|Sales|Human Resources|Operations|Marketing|Manufacturing|Website|Theme|Administration|Appraisals|Sign|Helpdesk|Administration|Extra Rights|Other Extra Rights|
+    'category': 'Event', # Technical Settings|Localization|Payroll Localization|Account Charts|User types|Invoicing|Sales|Human Resources|Operations|Marketing|Manufacturing|Website|Theme|Administration|Appraisals|Sign|Helpdesk|Administration|Extra Rights|Other Extra Rights|
     'description': """
-        Long description of module's purpose
+    
     """,
     #'sequence': 1,
     'author': 'Vertel AB',
