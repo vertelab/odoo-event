0ae21a3cc939edb8ff3aefd429408e2ccb123dbf
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2024-08-20 08:56:28 +0200

    Update __manifest__.py

--- website_event_portal_elearning/__manifest__.py
+++ website_event_portal_elearning/__manifest__.py
@@ -24,9 +24,7 @@
 {
     'name': 'Event: Website Event Portal Elearning Glue',
     'version': '0.1',
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.odoo.com""",
+    'summary': '',
     'category': 'Event', # Technical Settings|Localization|Payroll Localization|Account Charts|User types|Invoicing|Sales|Human Resources|Operations|Marketing|Manufacturing|Website|Theme|Administration|Appraisals|Sign|Helpdesk|Administration|Extra Rights|Other Extra Rights|
     'description': """
     
