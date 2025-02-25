6406dca031c043ec55b75a5dea99ca354b4fe186
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-02-25 11:04:16 +0100

    Update __manifest__.py

--- website_event_waitlist/__manifest__.py
+++ website_event_waitlist/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Event: Website Event Waitlist',
-    'version': '17.0.0.1.0',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Enhance event with waiting list functionality.',
     # Categories can be used to filter modules in modules listing
