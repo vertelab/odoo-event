4d03b52580bec4073319107589aec9b6c03fa8dc
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-02-26 16:09:52 +0100

    Update __manifest__.py

--- event_hr_requirement/__manifest__.py
+++ event_hr_requirement/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Event: Requirements',
-    'version': '17.0.1.1.0',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'To be able to have multiple partners on event.',
     # Categories can be used to filter modules in modules listing
