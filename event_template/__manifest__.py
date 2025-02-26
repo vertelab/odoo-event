14449ae185ec571da156f1ed5b913eb82904d13c
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-02-26 16:19:26 +0100

    Update __manifest__.py

--- event_template/__manifest__.py
+++ event_template/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Event: Event Template',
-    'version': '17.0.1.3.0',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'To be able to create event from another event.',
     # Categories can be used to filter modules in modules listing
