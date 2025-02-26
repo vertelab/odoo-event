e096953e0e2dd1e0285766631c70e233a6af4bb1
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-02-26 16:10:22 +0100

    Update __manifest__.py

--- event_lxp/__manifest__.py
+++ event_lxp/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Event: Learning Experience Platform',
-    'version': '17.0.0.1',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Adds AI-driven functionality to the LX-plattform in Event/e-learning.',
     # Categories can be used to filter modules in modules listing
