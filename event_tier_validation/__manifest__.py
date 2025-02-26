fd00295155f7b6ed846593e03b93f28787d856d7
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-02-26 16:20:07 +0100

    Update __manifest__.py

--- event_tier_validation/__manifest__.py
+++ event_tier_validation/__manifest__.py
@@ -23,7 +23,7 @@
 #
 {
     'name': 'Event: Tier Validation',
-    'version': '17.0.0.0.0',
+    'version': '1.0',
     'summary': 'Extends the functionality of Event Type to support a tier validation process.',
     'category': 'Event',
     'description': """
