be2ed1141449a0373a5049180e011f7ec9b8c4ad
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-02-26 16:21:54 +0100

    Update __manifest__.py

--- website_event_sale_status/__manifest__.py
+++ website_event_sale_status/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Event: Website Event Sale Tentative Status',
-    'version': '17.0.0.0.0',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Enhance event and sale with functionality.',
     # Categories can be used to filter modules in modules listing
