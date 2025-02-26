b2d0153d7981fe9c5d3b8c7fd3a16a3a6fefc962
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-02-26 16:17:08 +0100

    Update __manifest__.py

--- event_partner_calendar/__manifest__.py
+++ event_partner_calendar/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Event: Partner Calendar',
-    'version': '17.0.0.0.2',
+    'version': '1.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'New events are shown in the calendar.',
     # Categories can be used to filter modules in modules listing
