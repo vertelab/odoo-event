0ebb20f3b1b3badf664c1f64e6cf0297875eb3c7
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2025-02-26 16:23:11 +0100

    Update __manifest__.py

--- event_partner_foodallergy/__manifest__.py
+++ event_partner_foodallergy/__manifest__.py
@@ -21,7 +21,7 @@
 
 {
     'name': 'Event: Partner Foodallergy',
-    'version': '17.0.0.3',
+    'version': '1.3',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
     'summary': 'Extends the event registration form view with an option about foodallergy.',
     # Categories can be used to filter modules in modules listing
