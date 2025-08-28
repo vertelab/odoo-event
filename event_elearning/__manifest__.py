04cde686fa2427ad4f2a67bda63e4bfabd3059c2
marcuszxc <marcus.karlsson@vertel.se>
2025-08-28 07:04:53 +0000

    Removed link that allowed one to part taking in course that you did not meet the prerequisite for

--- event_elearning/__manifest__.py
+++ event_elearning/__manifest__.py
@@ -35,11 +35,12 @@
     """,
     'depends': ['hr','event','website_slides'],
     'data': [
+        'security/security.xml',
         'data/event_reminder_mail_templet.xml',
         'data/event_subscription_mail_templet.xml',
         'views/event_event.xml',
         'views/menu.xml',
-        'security/security.xml',
+        'views/website_slides_templates_course.xml',
     ],
     'demo': [],
     'qweb': [],
