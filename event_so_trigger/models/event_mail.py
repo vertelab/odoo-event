98a6865bb652d3bf8b3bf096e9be68e4f5716c63
iammiracle <miracleaayodele@gmail.com>
2026-02-23 10:17:04 +0100

    improve event communication triggers

--- event_so_trigger/models/event_mail.py
+++ event_so_trigger/models/event_mail.py
@@ -12,55 +12,76 @@ class TriggerEvent(models.Model):
         ],
         ondelete={'after_confirmed_so': 'cascade', 'after_reservation_reg': 'cascade'})
 
+    @api.depends('event_id.date_begin', 'event_id.date_end', 'interval_type', 'interval_unit', 'interval_nbr')
     def _compute_scheduled_date(self):
-        for mail in self:
-            if mail.interval_type in ['after_sub', 'after_confirmed_so', 'after_reservation_reg']:
-                date, sign = mail.event_id.create_date, 1
-            elif mail.interval_type == 'before_event':
-                date, sign = mail.event_id.date_begin, -1
+        for scheduler in self:
+            if scheduler.interval_type in ['after_sub', 'after_confirmed_so', 'after_reservation_reg']:
+                date, sign = scheduler.event_id.create_date, 1
+            elif scheduler.interval_type == 'before_event':
+                date, sign = scheduler.event_id.date_begin, -1
             else:
-                date, sign = mail.event_id.date_end, 1
+                date, sign = scheduler.event_id.date_end, 1
 
-            mail.scheduled_date = date + _INTERVALS[mail.interval_unit](sign * mail.interval_nbr) if date else False
+            scheduler.scheduled_date = date.replace(microsecond=0) + _INTERVALS[scheduler.interval_unit](sign * scheduler.interval_nbr) if date else False
+
+    @api.depends('interval_type', 'mail_done')
+    def _compute_mail_state(self):
+        for scheduler in self:
+            if scheduler.interval_type in ('after_sub', 'after_confirmed_so', 'after_reservation_reg'):
+                scheduler.mail_state = 'running'
+            elif scheduler.mail_done:
+                scheduler.mail_state = 'sent'
+            else:
+                scheduler.mail_state = 'scheduled'
 
     def execute(self):
-        for mail in self:
-            now = fields.Datetime.now()
-            if mail.interval_type == 'after_sub':
-                # update registration lines
-                lines = [
-                    (0, 0, {'registration_id': registration.id})
-                    for registration in (mail.event_id.registration_ids - mail.mapped('mail_registration_ids.registration_id'))
-                ]
-                if lines:
-                    mail.write({'mail_registration_ids': lines})
-                # execute scheduler on registrations
-                mail.mail_registration_ids.execute()
-            elif mail.interval_type == 'after_confirmed_so':
-                lines = [
-                    (0, 0, {'registration_id': registration.id})
-                    for registration in mail.event_id.registration_ids - mail.mapped('mail_registration_ids.registration_id')
-                    if registration.sale_order_id.state in ['sale', 'done']
-                ]
-                if lines:
-                    mail.write({'mail_registration_ids': lines})
-                mail.mail_registration_ids.execute()
-            elif mail.interval_type == 'after_reservation_reg':
-                lines = [
-                    (0, 0, {'registration_id': registration.id})
-                    for registration in mail.event_id.registration_ids.filtered(
-                        lambda reg: reg.state == 'reservation') - mail.mapped('mail_registration_ids.registration_id')
-                ]
-                if lines:
-                    mail.write({'mail_registration_ids': lines})
-                mail.mail_registration_ids.execute()
+        now = fields.Datetime.now()
+        for scheduler in self._filter_template_ref():
+            if scheduler.interval_type == 'after_sub':
+                scheduler._execute_attendee_based()
+            elif scheduler.interval_type == 'after_confirmed_so':
+                scheduler._execute_confirmed_so_based()
+            elif scheduler.interval_type == 'after_reservation_reg':
+                scheduler._execute_reservation_based()
             else:
-                # Do not send emails if the mailing was scheduled before the event but the event is over
-                if not mail.mail_done and mail.scheduled_date <= now and mail.notification_type == 'mail' and \
-                        (mail.interval_type != 'before_event' or mail.event_id.date_end > now):
-                    mail.event_id.mail_attendees(mail.template_ref.id)
-                    # mail.write({'mail_sent': True}) - Invalid field 'mail_sent' on model 'event.mail'
+                # before_event / after_event — single-shot
+                if scheduler.mail_done:
+                    continue
+                if scheduler.scheduled_date <= now and (
+                    scheduler.interval_type != 'before_event' or scheduler.event_id.date_end > now
+                ):
+                    scheduler._execute_event_based()
         return True
+
+    def _execute_confirmed_so_based(self):
+        self.ensure_one()
+        registrations = self.event_id.registration_ids.filtered(
+            lambda r: r.sale_order_id.state in ('sale', 'done')
+        )
+        self._execute_registration_based(registrations)
+
+    def _execute_reservation_based(self):
+        self.ensure_one()
+        registrations = self.event_id.registration_ids.filtered(
+            lambda r: r.state == 'reservation'
+        )
+        self._execute_registration_based(registrations)
+
+    def _execute_registration_based(self, registrations):
+        self.ensure_one()
+        already_linked = self.mapped('mail_registration_ids.registration_id')
+        new_registrations = registrations - already_linked
+
+        if new_registrations:
+            self._create_missing_mail_registrations(new_registrations)
+
+        self.with_context(
+            event_mail_registration_ids=registrations.ids
+        ).mail_registration_ids.filtered(
+            lambda r: r.registration_id in registrations
+        ).execute()
+
+        self._refresh_mail_count_done()
 
 
 class TriggerEventType(models.Model):
@@ -77,62 +98,35 @@ class EventRegistration(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-        logging.warning(f"mar /usr/share/odoo-event/event_so_trigger/models/event_mail.py {vals_list=}")
-        registrations = super(EventRegistration, self).create(vals_list)
-        for reg in registrations:
-            if reg.state == 'reservation':
-                onsubscribe_schedulers = reg.mapped('event_id.event_mail_ids').filtered(
-                    lambda s: s.interval_type == 'after_reservation_reg'
-                )
-                onsubscribe_schedulers.with_user(SUPERUSER_ID).execute()
-
+        registrations = super().create(vals_list)
+        reservation_regs = registrations.filtered(lambda r: r.state == 'reservation')
+        if reservation_regs:
+            schedulers = reservation_regs.mapped('event_id.event_mail_ids').filtered(
+                lambda s: s.interval_type == 'after_reservation_reg'
+            )
+            schedulers.with_user(SUPERUSER_ID).with_context(
+                event_mail_registration_ids=reservation_regs.ids
+            ).execute()
         return registrations
 
     def write(self, vals):
-        ret = super(EventRegistration, self).write(vals)
-
+        ret = super().write(vals)
         if vals.get('state') == 'open':
-            # auto-trigger after_sub (on subscribe) mail schedulers, if needed
-            onsubscribe_schedulers = self.mapped('event_id.event_mail_ids').filtered(
-                lambda s: s.interval_type in ['after_sub', 'after_confirmed_so']
+            schedulers = self.mapped('event_id.event_mail_ids').filtered(
+                lambda s: s.interval_type in ('after_sub', 'after_confirmed_so')
             )
-            onsubscribe_schedulers.with_user(SUPERUSER_ID).execute()
-
+            schedulers.with_user(SUPERUSER_ID).with_context(
+                event_mail_registration_ids=self.ids
+            ).execute()
         return ret
 
 
 class EventMailRegistration(models.Model):
     _inherit = "event.mail.registration"
 
-    def execute(self):
-        now = fields.Datetime.now()
-        todo = self.filtered(lambda reg_mail:
-            not reg_mail.mail_sent and \
-            reg_mail.registration_id.state in ['open', 'done'] and \
-            (reg_mail.scheduled_date and reg_mail.scheduled_date <= now) and \
-            reg_mail.scheduler_id.notification_type == 'mail'
-        )
-        todo += self.filtered(lambda reg_mail:
-             not reg_mail.mail_sent and \
-             reg_mail.registration_id.state == 'reservation' and \
-             (reg_mail.scheduled_date and reg_mail.scheduled_date <= now) and \
-             reg_mail.scheduler_id.notification_type == 'mail' and reg_mail.scheduler_id.interval_type == 'after_reservation_reg'
-         )
-        for reg_mail in set(todo):
-            organizer = reg_mail.scheduler_id.event_id.organizer_id
-            company = self.env.company
-            author = self.env.ref('base.user_root')
-            if organizer.email:
-                author = organizer
-            elif company.email:
-                author = company.partner_id
-            elif self.env.user.email:
-                author = self.env.user
-
-            email_values = {
-                'author_id': author.id,
-            }
-            if not reg_mail.scheduler_id.template_ref.email_from:
-                email_values['email_from'] = author.email_formatted
-            reg_mail.scheduler_id.template_ref.send_mail(reg_mail.registration_id.id, email_values=email_values)
-        todo.write({'mail_sent': True})
+    def _get_skip_domain(self):
+        domain = super()._get_skip_domain()
+        if any(r.scheduler_id.interval_type == 'after_reservation_reg' for r in self):
+            domain = [d for d in domain if 'registration_id.state' not in str(d)]
+            domain += [('registration_id.state', 'in', ('open', 'done', 'reservation'))]
+        return domain
